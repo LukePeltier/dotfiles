506dd2cdd0b24d2c8d8531ce140b486b6ce3bde4
Luke Peltier <luke@lukepeltier.com>
2024-05-28 13:13:26 -0500

    Add wezterm config

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,10 @@
+local wezterm = require("wezterm")
+
+return {
+	color_scheme = "Catppuccin Mocha",
+	enable_tab_bar = false,
+	font_size = 12.0,
+	font = wezterm.font("JetBrainsMono Nerd Font"),
+	window_close_confirmation = "NeverPrompt",
+	default_domain = "WSL:Mint",
+}
