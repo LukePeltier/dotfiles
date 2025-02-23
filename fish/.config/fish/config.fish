a65f19775ae8d7b9b916499ace9dddadd110ae39
Luke Peltier <luke@lukepeltier.com>
2025-02-23 15:31:51 -0600

    Updates

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,18 +1,24 @@
 if status is-interactive
   # Commands to run in interactive sessions can go here
   # atuin init fish --disable-up-arrow | source
+
+
   zoxide init fish --cmd cd | source
   bind \cf 'zellij-sessionizer'
 
   set -gx BAT_THEME kanagawa
-  set -gx MANPAGER "sh -c 'col -bx | bat -l man -p'"
+  set -gx MANPAGER "sh -c 'col -bx | batcat -l man -p'"
   set -gx MANROFFOPT -c
   # BEGIN opam configuration
   test -r "$HOME/.opam/opam-init/init.fish" && source "$HOME/.opam/opam-init/init.fish" > /dev/null 2> /dev/null; or true
   # END opam configuration
 
   test -r "$HOME/.asdf/asdf.fish" && source "$HOME/.asdf/asdf.fish" > /dev/null 2> /dev/null; or true
-   test -r "$HOME/.asdf/plugins/golang/set-env.fish" && source "$HOME/.asdf/plugins/golang/set-env.fish"  > /dev/null 2> /dev/null; or true
+  test -r "$HOME/.asdf/plugins/golang/set-env.fish" && source "$HOME/.asdf/plugins/golang/set-env.fish"  > /dev/null 2> /dev/null; or true
+
+  set -gx ZVM_INSTALL "$HOME/.zvm/self"
+  fish_add_path "$HOME/.zvm/bin"
+  fish_add_path "$ZVM_INSTALL"
 end
 
 
