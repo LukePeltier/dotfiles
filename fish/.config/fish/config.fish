6edbbfde7dff19b395438c55a595ec6ad9fbe497
Luke Peltier <Luke.Peltier@microchip.com>
2025-03-31 13:01:23 -0500

    Move to conf.d

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -14,25 +14,8 @@ if status is-interactive
   set -gx BAT_THEME kanagawa
   set -gx MANPAGER "sh -c 'col -bx | batcat -l man -p'"
   set -gx MANROFFOPT -c
-  # BEGIN opam configuration
-  test -r "$HOME/.opam/opam-init/init.fish" && source "$HOME/.opam/opam-init/init.fish" > /dev/null 2> /dev/null; or true
-  # END opam configuration
-
-  # test -r "$HOME/.asdf/asdf.fish" && source "$HOME/.asdf/asdf.fish" > /dev/null 2> /dev/null; or true
-  # test -r "$HOME/.asdf/plugins/golang/set-env.fish" && source "$HOME/.asdf/plugins/golang/set-env.fish"  > /dev/null 2> /dev/null; or true
-  test -r "$HOME/.local/bin/mise" && mise activate fish | source
-
-  test -r "$HOME/.deno/env.fish" && source "$HOME/.deno/env.fish"
-
-  set -gx ZVM_INSTALL "$HOME/.zvm/self"
-  fish_add_path "$HOME/.zvm/bin"
-  fish_add_path "$ZVM_INSTALL"
 
   ssh-add > /dev/null 2>/dev/null
 
 end
 
-
-
-
-uv generate-shell-completion fish | source
