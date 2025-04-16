cbab3bb9bfdcb035c3d0e3612130186621269e25
Luke Peltier <luke@lukepeltier.com>
2025-04-16 10:14:33 -0500

    automated dev commit

--- env/.config/fish/conf.d/atuin.env.fish
+++ env/.config/fish/conf.d/atuin.env.fish
@@ -1,7 +1,9 @@
 
-source "$HOME/.atuin/bin/env.fish"
-
-set -gx ATUIN_NOBIND "true"
-atuin init fish | source
-
-bind \ct _atuin_search
+if test "$HOME/.atuin"
+  source "$HOME/.atuin/bin/env.fish"
+  
+  set -gx ATUIN_NOBIND "true"
+  atuin init fish | source
+  
+  bind \ct _atuin_search
+end
