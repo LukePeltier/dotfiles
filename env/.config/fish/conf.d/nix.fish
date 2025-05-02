d515d97827efcc9f680272df7802ee6ca18623ef
Luke Peltier <luke@lukepeltier.com>
2025-05-02 10:43:30 -0500

    automated dev commit

--- env/.config/fish/conf.d/nix.fish
+++ env/.config/fish/conf.d/nix.fish
@@ -1,4 +1,3 @@
 if test -r '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish'
   . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish'
-  replay $HOME/.nix-profile/etc/profile.d/hm-session-vars.sh
 end
