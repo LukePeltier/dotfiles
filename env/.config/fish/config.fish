408d6014077429629c4e75dbaa143d9234a26c9f
Luke Peltier <luke@lukepeltier.com>
2025-05-08 18:42:43 -0500

    automated dev commit

--- env/.config/fish/config.fish
+++ env/.config/fish/config.fish
@@ -1,5 +1,14 @@
 if status is-interactive
 
+  # Only run on TTY1
+  if test (tty) = "/dev/tty1"
+    if command -v uwsm >/dev/null
+      if uwsm check may-start
+        exec uwsm start hyprland.desktop
+      end
+    end
+  end
+
   fish_add_path "$HOME/.local/bin"
   fish_add_path "$HOME/bin"
   fish_add_path "/opt/homebrew/opt/curl/bin"
