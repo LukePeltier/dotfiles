442eb5cd8ae5100cb3babddb5c3f802866a57559
Luke Peltier <luke@lukepeltier.com>
2025-09-22 20:16:06 -0500

    Moving to plasma

---
+++ env/.config/fish/conf.d/wayland.fish
@@ -0,0 +1,17 @@
+# Set the cursor size for xcursor
+set -gx XCURSOR_SIZE 24
+set -gx HYPRCURSOR_SIZE 24
+
+# XDG Desktop Portal
+set -gx XDG_SESSION_TYPE wayland
+set -gx XDG_MENU_PREFIX arch-
+
+set -gx QT_QPA_PLATFORM wayland
+set -gx SDL_VIDEODRIVER wayland
+set -gx MOZ_ENABLE_WAYLAND 1
+set -gx ELECTRON_OZONE_PLATFORM_HINT wayland
+set -gx OZONE_PLATFORM wayland
+
+# Use XCompose file
+set -gx XCOMPOSEFILE $HOME/.XCompose
+
