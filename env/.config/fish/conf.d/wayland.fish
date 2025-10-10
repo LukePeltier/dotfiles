73a8c523e701a9fc58efe878ddaa8e13655a488d
Luke Peltier <luke@lukepeltier.com>
2025-10-10 08:41:57 -0500

    Unify gtk and qt

--- env/.config/fish/conf.d/wayland.fish
+++ env/.config/fish/conf.d/wayland.fish
@@ -6,7 +6,6 @@ set -gx HYPRCURSOR_SIZE 24
 set -gx XDG_SESSION_TYPE wayland
 set -gx XDG_MENU_PREFIX arch-
 
-set -gx QT_QPA_PLATFORM wayland
 set -gx SDL_VIDEODRIVER wayland
 set -gx MOZ_ENABLE_WAYLAND 1
 set -gx ELECTRON_OZONE_PLATFORM_HINT wayland
@@ -15,4 +14,3 @@ set -gx OZONE_PLATFORM wayland
 # Use XCompose file
 set -gx XCOMPOSEFILE $HOME/.XCompose
 
-set -gx QT_STYLE_OVERRIDE kvantum
