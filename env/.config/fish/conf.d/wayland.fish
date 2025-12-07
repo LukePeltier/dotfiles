ca0ca4eb878bc5a62dd9a19bd9a1e6832dab5659
Luke Peltier <luke@lukepeltier.com>
2025-12-07 14:56:31 -0600

    Noctalia

--- env/.config/fish/conf.d/wayland.fish
+++ env/.config/fish/conf.d/wayland.fish
@@ -1,18 +1,1 @@
-# Set the cursor size for xcursor
-set -gx XCURSOR_SIZE 24
 
-# XDG Desktop Portal
-set -gx XDG_SESSION_TYPE wayland
-set -gx XDG_MENU_PREFIX arch-
-
-set -gx SDL_VIDEODRIVER wayland
-set -gx MOZ_ENABLE_WAYLAND 1
-set -gx ELECTRON_OZONE_PLATFORM_HINT wayland
-set -gx OZONE_PLATFORM wayland
-
-# Use XCompose file
-set -gx XCOMPOSEFILE $HOME/.XCompose
-
-set -gx QT_STYLE_OVERRIDE kvantum
-set -gx QT_QPA_PLATFORMTHEME qt5ct
-set -gx QT_AUTO_SCREEN_SCALE_FACTOR 1
