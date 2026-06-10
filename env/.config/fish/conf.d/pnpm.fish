e5e9f5f4eb5fc0d458f74294254e0d10301193b8
Luke Peltier <luke@lukepeltier.com>
2026-06-10 16:51:15 -0500

    fish: fix pnpm PATH to use PNPM_HOME/bin

--- env/.config/fish/conf.d/pnpm.fish
+++ env/.config/fish/conf.d/pnpm.fish
@@ -1,6 +1,6 @@
 # pnpm
 set -gx PNPM_HOME "$HOME/.local/share/pnpm"
-if test -d "$PNPM_HOME"; and not string match -q -- $PNPM_HOME $PATH
-    set -gx PATH "$PNPM_HOME" $PATH
+if test -d "$PNPM_HOME/bin"; and not string match -q -- "$PNPM_HOME/bin" $PATH
+    set -gx PATH "$PNPM_HOME/bin" $PATH
 end
 # pnpm end
